41d916052a7de18818d60e2dec28a3be4a3d79d8
이준희 <junh0328@naver.com>
2020-11-20 16:10:39 +0900

    기본 UI 구성 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Counter from './components/Counter';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </>
   );
 }
