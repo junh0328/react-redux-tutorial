0444bcb4915b0d5f843f45fab0041ba7b7bee338
이준희 <junh0328@naver.com>
2020-11-30 15:14:26 +0900

    Todo 프레젠테이셔널 컴포넌트에서 TodoItem 분리

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,21 +1,5 @@
 import React from 'react';
-
-const TodoItem = ({ todo, onToggle, onRemove }) => {
-  return (
-    <div>
-      <input
-        type="checkbox"
-        onClick={() => onToggle(todo.id)}
-        checked={todo.checked}
-        readOnly={true}
-      />
-      <span style={{ textDecoration: todo.checked ? 'line-through' : ' none' }}>
-        {todo.text}
-      </span>
-      <button onClick={() => onRemove(todo.id)}>삭제</button>
-    </div>
-  );
-};
+import TodoItem from './TodoItem';
 
 const Todos = ({
   input, // 인풋에 입력되는 텍스트
